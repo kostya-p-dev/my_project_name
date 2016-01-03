ac56118c0c9a1b86a07e5ebdb0ffd1b99320ca51
Kostya <ermine.kostya@gmail.com>
2016-01-03 13:59:02 +0200

    test email

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -25,66 +25,63 @@ class DefaultController extends Controller
      */
     public function emailAction(Request $request)
     {
-        // получаем 'mailer' (обязателен для инициализации Swift Mailer)
-//        $mailer = $this->get('mailer');
-//
-//        $message = \Swift_Message::newInstance()
-//            ->setSubject('SYsky2 #'. rand(1000000000, 1000000000000))
-//            ->setFrom('ermine.test1@gmail.com')
-//            ->setTo('kostya.p.dev@gmail.com')
-//            ->setBody($this->renderView('Emails/registration.html.twig', array('name' => 'TEST')))
-//            ->setContentType('text/html')
-//        ;
-//        $resp = $mailer->send($message);
-//
-//        var_dump($resp);
-//        return new Response('Hello world!');
+        $to = "kostya.p.dev@gmail.com";
+        $subject = "HTML email";
+
+        $message = "
+            <html>
+            <head>
+            <title>HTML email</title>
+            </head>
+            <body>
+            <p>This email contains HTML Tags!</p>
+            <table>
+            <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            </tr>
+            <tr>
+            <td>John</td>
+            <td>Doe</td>
+            </tr>
+            </table>
+            </body>
+            </html>
+            ";
+
+// Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+        $headers .= 'From: <webmaster@example.com>' . "\r\n";
+        $headers .= 'Cc: myboss@example.com' . "\r\n";
 
 
-
-//        $to      = 'kostya.p.dev@gmail.com';
-//        $from    = 'kostya.p.dev@gmail.com';
-//        $subject = 'New Order! #' .rand(1000, 100000);
-//        $message = $this->renderView('Emails/registration.html.twig', array('name' => 'TEST'));
-//
-//        $res = $this->send_mail($to,$from,$subject,$message);
-//
-//        var_dump($res);
-//        return new Response('Hello world!');
-
-        $to      = 'kostya.p.dev@gmail.com';
-        $subject = 'the subject';
-        $message = 'hello';
-        $headers = 'From: webmaster@example.com' . "\r\n" .
-            'Reply-To: webmaster@example.com' . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
-
-
-
-        $res = mail($to, $subject, $message, $headers);
+        $res =  mail($to,$subject,$message,$headers);
         var_dump($res);
         return new Response('Hello world!');
     }
 
     public function send_mail($to,$from,$subject,$msg){
 
-        $headers ="MIME-Version: 1.0 \r\n";
-        $headers.="from: $from \r\n";
-        $headers.="Reply-To: $from" . "\r\n" .
-        $headers.="Content-type: text/html;charset=utf-8 \r\n";
-        $headers.="X-Priority: 3\r\n";
-        $headers.="X-Mailer: smail-PHP ".phpversion()."\r\n";
-        $msg    ='
-            <div style="text-align:left">
-            <h2>'.$subject.'</h2>
-            '.$msg.'
-            </div>
-            ';
-
-        if( mail($to,$subject,$msg,$headers) ){
-            return true;
-        }else{
-            return false;
-        }
+//        $headers ="MIME-Version: 1.0 \r\n";
+//        $headers.="from: $from \r\n";
+//        $headers.="Reply-To: $from" . "\r\n" .
+//        $headers.="Content-type: text/html;charset=utf-8 \r\n";
+//        $headers.="X-Priority: 3\r\n";
+//        $headers.="X-Mailer: smail-PHP ".phpversion()."\r\n";
+//        $msg    ='
+//            <div style="text-align:left">
+//            <h2>'.$subject.'</h2>
+//            '.$msg.'
+//            </div>
+//            ';
+//
+//        if( mail($to,$subject,$msg,$headers) ){
+//            return true;
+//        }else{
+//            return false;
+//        }
     }
 }
