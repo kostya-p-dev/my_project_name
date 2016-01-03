85ee1349bda862b75f7001baeb587785e59de2fc
Kostya <ermine.kostya@gmail.com>
2016-01-03 13:13:25 +0200

    test email

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -26,18 +26,52 @@ class DefaultController extends Controller
     public function emailAction(Request $request)
     {
         // получаем 'mailer' (обязателен для инициализации Swift Mailer)
-        $mailer = $this->get('mailer');
+//        $mailer = $this->get('mailer');
+//
+//        $message = \Swift_Message::newInstance()
+//            ->setSubject('SYsky #'. rand(1000000000, 1000000000000))
+//            ->setFrom('ermine.test1@gmail.com')
+//            ->setTo('kostya.p.dev@gmail.com')
+//            ->setBody($this->renderView('Emails/registration.html.twig', array('name' => 'TEST')))
+//            ->setContentType('text/html')
+//        ;
+//        $resp = $mailer->send($message);
+//
+//        var_dump($resp);
+//        return new Response('Hello world!');
 
-        $message = \Swift_Message::newInstance()
-            ->setSubject('SYsky #'. rand(1000000000, 1000000000000))
-            ->setFrom('ermine.test1@gmail.com')
-            ->setTo('kostya.p.dev@gmail.com')
-            ->setBody($this->renderView('Emails/registration.html.twig', array('name' => 'TEST')))
-            ->setContentType('text/html')
-        ;
-        $resp = $mailer->send($message);
 
-        var_dump($resp);
+
+        $to      = 'kostya.p.dev@gmail.com';
+        $from    = 'kostya.p.dev@gmail.com';
+        $subject = 'New Order! #' .rand(1000, 100000);
+        $message = $this->renderView('Emails/registration.html.twig', array('name' => 'TEST'));
+
+        $res = $this->send_mail($to,$from,$subject,$message);
+
+        var_dump($res);
         return new Response('Hello world!');
+    }
+
+    public function send_mail($to,$from,$subject,$msg){
+
+        $headers ="MIME-Version: 1.0 \r\n";
+        $headers.="from: $from \r\n";
+        $headers.="Reply-To: $from" . "\r\n" .
+        $headers.="Content-type: text/html;charset=utf-8 \r\n";
+        $headers.="X-Priority: 3\r\n";
+        $headers.="X-Mailer: smail-PHP ".phpversion()."\r\n";
+        $msg    ='
+            <div style="text-align:left">
+            <h2>'.$subject.'</h2>
+            '.$msg.'
+            </div>
+            ';
+
+        if( mail($to,$subject,$msg,$headers) ){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
